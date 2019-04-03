b7d8d07aafae2d198b85a05dea9a508792ec178e
Stephanie Guevara <stephanie.estela.guevara.gullo@everis.com>
2019-04-03 17:13:27 +0200

    data form

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
-  styleUrls: ['./data.component.scss']
+  styles: [`
+    .red {
+      border: 1px solid red;
+    }`]
 })
 export class DataComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({
+      'name': new FormControl('', [
+                                    Validators.required,
+                                    Validators.minLength(3)
+                                  ]),
+      'lastName': new FormControl('', Validators.required  ),
+      'email': new FormControl('', [
+                                      Validators.required,
+                                      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+                                    ]     )
+    });
+   }
 
   ngOnInit() {
+  }
+
+  saveInfo(){
+    console.log(this.form.value);
   }
 
 }
