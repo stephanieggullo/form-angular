b03b68e56cedb6c160a46c9e82f6c045b7e4c988
Stephanie Guevara <stephanie.estela.guevara.gullo@everis.com>
2019-04-04 10:05:43 +0200

    password

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -15,31 +15,51 @@ export class DataComponent implements OnInit {
   form: FormGroup;
 
   user: object = {
-    name: 'Steph',
-    lastName: 'Guevara',
+    name: '',
+    lastName: '',
     email: null,
   };
 
   constructor() {
     this.form = new FormGroup({
       'name': new FormControl('', [
-                                    Validators.required,
-                                    Validators.minLength(3)
-                                  ]),
-      'lastName': new FormControl('', Validators.required  ),
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      'lastName': new FormControl('', Validators.required),
       'email': new FormControl('', [
-                                      Validators.required,
-                                      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
-                                    ]     )
+        Validators.required,
+        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+      ]),
+      'password1': new FormControl('', Validators.required),
+      'password2': new FormControl()
+
     });
 
-    this.form.setValue(this.user);
-   }
+    this.form.controls['password2'].setValidators([
+      Validators.required,
+      this.notEqual.bind(this.form)
+    ]);
+
+    // this.form.setValue(this.user);
+  }
+
+  notEqual(control: FormControl): { [s: string]: boolean } {
+
+    let form: any = this;
+    if (control.value !== form.controls['password1'].value) {
+      return {
+        noequal: true
+      }
+    }
+
+    return null;
+  }
 
   ngOnInit() {
   }
 
-  saveInfo(){
+  saveInfo() {
     console.log(this.form.value);
   }
 
